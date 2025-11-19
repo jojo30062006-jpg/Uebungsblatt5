b2f2c51cf2e235ca86721ed44ce438ddd8543d81
Josephina Krause <josephinakrause@MacBook-Air-von-Josephina.local>
2025-11-19 21:16:38 +0100

    fertig

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -5,7 +5,7 @@ public class H3_main {
 	
 		int [][] einheiten = {{50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1},
 		{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
-		int input = 1000000;
+		int input = 6767676;
 		
 		System.out.println("Betrag: " + input + " Cent");
 		
